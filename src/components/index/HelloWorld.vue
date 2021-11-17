fb383bcdbf44d0d6809dbd2c6afbf1bc68155713
Jianting Yu <2226813074@qq.com>
2021-11-17 14:00:32 +0800

    first commit

---
+++ src/components/index/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <span @click="onClick">点击</span>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  components: {
+  },
+  created () {},
+  data () {
+    return {}
+  },
+  methods: {
+    onClick () {
+      axios.get('k8s.1400720231.cn/api/index').then((res) => {
+        console.log('数据：', res)
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
